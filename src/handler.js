269cda97024b56fb6b7ad8911fe7661f2e911844
Mufti Miftahul Kudus <muftimiftahul02@gmail.com>
2022-03-10 13:44:07 +0700

    fix error code

--- src/handler.js
+++ src/handler.js
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 /* eslint-disable linebreak-style */
 /* eslint-disable consistent-return */
@@ -113,30 +115,32 @@ const editNoteByIdHandler = (request, h) => {
     response.code(404);
     return response;
 
-    const deleteNoteByIdHandler = (request, h) => {
-        const { id } = request.params;
-       
-        const index = notes.findIndex((note) => note.id === id);
-       
-        if (index !== -1) {
-          notes.splice(index, 1);
-          const response = h.response({
-            status: 'success',
-            message: 'Catatan berhasil dihapus',
-          });
-          response.code(200);
-          return response;
-        }
-       
-       const response = h.response({
-          status: 'fail',
-          message: 'Catatan gagal dihapus. Id tidak ditemukan',
-        });
-        response.code(404);
-        return response;
-      };
     
 };
+
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+   
+    const index = notes.findIndex((note) => note.id === id);
+   
+    if (index !== -1) {
+      notes.splice(index, 1);
+      const response = h.response({
+        status: 'success',
+        message: 'Catatan berhasil dihapus',
+      });
+      response.code(200);
+      return response;
+    }
+   
+   const response = h.response({
+      status: 'fail',
+      message: 'Catatan gagal dihapus. Id tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+};
+  
 module.exports = {
     addNoteHandler,
     getAllNotesHandler,
